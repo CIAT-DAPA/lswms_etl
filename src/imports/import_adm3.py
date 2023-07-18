40615dffaee59f3f01bf9b0a7d975b136457b168
CarlosNasayo <carlosnasayov@gmail.com>
2023-07-18 16:13:37 -0500

    EWP-28 Added an error log in the import of ADM3, if there is an error, create an error folder and save the log in TXT

--- src/imports/import_adm3.py
+++ src/imports/import_adm3.py
@@ -1,55 +1,60 @@
-import os, sys
+import os
+import sys
+import geopandas as gpd
+from datetime import datetime
+from mongoengine import connect
+from ormWP import Adm1, Adm2, Adm3
+
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 from parameters.get_connection import *
 from parameters.get_file import *
 from imports.import_dataframe import *
-import os
-import sys
-from mongoengine import connect
-import geopandas as gpd
-from datetime import datetime
-from ormWP import Adm1
-from ormWP import Adm2
-from ormWP import Adm3
+
+def log_error(message):
+    # Create an "error" folder if it doesn't exist
+    error_folder = os.path.join(os.path.dirname(__file__), '..', 'error')
+    os.makedirs(error_folder, exist_ok=True)
+
+    # Log the error message to a file
+    error_log_file = os.path.join(error_folder, 'error_log.txt')
+    with open(error_log_file, 'a') as f:
+        f.write(f"{datetime.now()}: {message}\n")
+
+def etl_adm3(dataframe, cols, cols_kebele):
+    count = 0
+    try:
+        print('The process of importing kebeles has begun')
+        df_kebele = dataframe[cols].drop_duplicates()
+        df_kebele.columns = cols_kebele
+        print('Dimension', df_kebele.shape)
+        for index, row in df_kebele.iterrows():
+            ext_id = str(row['ext_id'])
+            if not Adm3.objects(ext_id=ext_id):
+                traced_list = [{"created": datetime.now(), "updated": datetime.now(), "enabled": True}]
+                print('importing', row['name'], ext_id)
+                adm2 = Adm2.objects.get(ext_id=str(row['adm3']))
+                if row['name'] is not None:
+                    adm3 = Adm3(name=str(row['name']), ext_id=ext_id, adm2=adm2, traced=traced_list, aclimate_id=str(row['aclimate_id']))
+                    adm3.save()
+                    count += 1
+            else:
+                print('not imported', row['name'], ext_id)
+        print(f'imported {count} kebeles to the database')
+    except Exception as e:
+        log_error(str(e))
 
 try:
     connect(host=get_mongo_conn_str())
-    print('connection established')
-except:
-    print('no connection')
-    print(host=get_mongo_conn_str())
+    print('Connection established')
+except Exception as e:
+    log_error(f'Error while establishing the connection with MongoDB: {str(e)}')
     sys.exit()
 
-def etl_adm3(dataframe,cols,cols_kebele):
-    count=0
-    print('The process of importing woredas has begun')
-    from datetime import datetime
-    df_kebele=dataframe[cols].drop_duplicates()
-    df_kebele.columns = cols_kebele
-    print('Dimension',df_kebele.shape)
-    for index, row in df_kebele.iterrows():
-         if not Adm3.objects(ext_id=str(row['ext_id'])):
-            traced_list = [
-                {"created": datetime.now(), "updated": datetime.now(), "enabled": True}
-            ]
-            print('importing',row['name'],row['ext_id'])
-            adm2=Adm2.objects.get(ext_id=str(row['adm3']))
-            if row['name'] is not None:
-                adm3=Adm3(name=str(row['name']),ext_id=str(row['ext_id']),adm2=adm2,traced=traced_list,aclimate_id=str(row['aclimate_id']))
-                adm3.save()
-                count+=1
-         else:
-            print('not imported',row['name'],row['ext_id'])
-    print(f'imported {count} kebeles to the database')
-    return
-    
-if get_complete_dataframe_to_import() is not None:
-    #define the columns of shape file to be imported code and name for the woreda and zone code
-    cols_kebele=['ext_id','name','adm3','aclimate_id']
-    cols=['id_adm4','name_adm4','id_adm3','ws_id']
-    
-    
-    dataframe=get_complete_dataframe_to_import()
-    etl_adm3(dataframe,cols,cols_kebele)
+dataframe = get_complete_dataframe_to_import()
+if dataframe is not None:
+    # define the columns of the shapefile to be imported, code, and name for the woreda and zone code
+    cols_kebele = ['ext_id', 'name', 'adm3', 'aclimate_id']
+    cols = ['id_adm4', 'name_adm4', 'id_adm3', 'ws_id']
+    etl_adm3(dataframe, cols, cols_kebele)
 else:
-    print('please check your shape file and path')
+    log_error('Please check your shapefile and path')

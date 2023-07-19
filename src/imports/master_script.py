0c3c6965e04a35050882528a86498d3f094aebc4
CarlosNasayo <carlosnasayov@gmail.com>
2023-07-19 11:27:39 -0500

    EWP-107 created a master script that executes in order all the import files for the ETL

---
+++ src/imports/master_script.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+def master_script(selected_files):
+    current_directory = os.getcwd()
+    files = os.listdir(current_directory)
+
+    python_files = [file for file in files if file.endswith('.py')]
+
+    for py_file in selected_files:
+        if py_file in python_files:
+            command = f'python "{py_file}"'
+            subprocess.run(command, shell=True)
+        else:
+            print(f"El archivo '{py_file}' no existe en el directorio actual.")
+
+if __name__ == "__main__":
+    # specific files to execute
+    selected_files_to_execute = ["import_adm1.py", "import_adm2.py","import_adm3.py","import_watershed.py"]
+
+    master_script(selected_files_to_execute)
